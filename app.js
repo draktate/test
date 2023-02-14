d27639a7d13b14115dd3347e15d189286909158f
draktate <draktate@gmail.com>
2023-02-14 13:39:30 +0530

    added new

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ app.get("/" , (req, res)=>{
 
     res.send("It works!")
 })
+
+app.get("/new" , (req, res)=>{
+
+    res.send("new works!")
+})
 app.listen(process.env.PORT||5000)
 module.exports=app
